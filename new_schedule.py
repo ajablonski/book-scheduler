a883768010858e86fbc9fe37858309d1c5309f5b
Alex Jablonski <alexmjablonski@gmail.com>
2013-06-27 22:49:14 -0500

    Make filename searching more robust, remove unneeded options

--- new_schedule.py
+++ new_schedule.py
@@ -19,14 +19,18 @@ parser.add_argument('-t', '--title',
 	help = 'Title of book')
 parser.add_argument('-o', '--output-file', type=os.path.normpath, default=OUTPUT_DEFAULT_NAME,
 	help = 'Output file name')
-parser.add_argument('-d', '--output-dir', default=os.getcwd() + '\\',
-	help = 'Output file dir')
 parser.add_argument('-p', '--stop-file', type=os.path.normpath, default=STOP_DEFAULT_NAME, required=True,
 	help = 'Stop file location')
 parser.add_argument('-c', '--columns', type=int, default=1,
 	help = 'Columns')
 
 args = parser.parse_args(sys.argv[1:])
+
+parser.add_argument('-d', '--output-dir', default=os.path.dirname(args.stop_file),
+	help = 'Output file dir')
+	
+args = parser.parse_args(sys.argv[1:])
+	
 book = BookSchedule(args.stop_file, args.start, args.end)
-book.pickle_to_file(args.output_dir + 'pickle.dat')
+book.pickle_to_file(os.path.join(args.output_dir, 'pickle.dat'))
 book.write_schedule_to_file(args.output_file, args.columns)
--- update_schedule.py
+++ update_schedule.py
@@ -5,26 +5,25 @@ import argparse
 
 parser = argparse.ArgumentParser(description = 'Update existing schedule for book')
 
-parser.add_argument('-i', '--df-name', default = 'pickle.dat', help = 'data file name')
-parser.add_argument('-d', '--df-dir', default = os.getcwd() + '\\', help = 'data file directory')
+parser.add_argument('-d', '--df', type = os.path.normpath, default = os.path.join(os.getcwd(), 'pickle.dat'), help = 'data file directory', required=True)
 parser.add_argument('-t', '--title', default = '', help = 'book title')
-parser.add_argument('-c', '--columns', default = 0, type=int,
+parser.add_argument('-c', '--columns', default = 1, type=int,
 	help='number of columns to show')
 parser.add_argument('-p', '--page', default = 0, type=int, help='current page of book')
 
-args = parser.parse_args(sys.argv)
+args = parser.parse_args(sys.argv[1:])
 
 if args.page == 0:
 	args.page = get_current_progress(args.title)[0]
 
-with open(args.df_dir + args.df_name, 'r') as data_file:
+with open(args.df, 'r') as data_file:
 	bookPart = pickle.load(data_file)
 
 book = BookSchedule(bookPart._stop_file_path, bookPart._start, bookPart._end)
 
-book.update_schedule(current_page)
+book.update_schedule(args.page)
 
-book.write_schedule_to_file(args.df_dir  + '/' + date.today().isoformat() + '.txt', args.columns)
+book.write_schedule_to_file(os.path.join(os.path.dirname(args.df), date.today().isoformat() + '.txt'), args.columns)
 
-with open(args.df_dir + args.df_name, 'w') as data_file:
+with open(args.df, 'w') as data_file:
 	pickle.dump(book, data_file)
